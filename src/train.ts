bda7fc2600999face109c75e6783bbd53cb9f8e3
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-06 20:32:19 -0800

    feat: signup process frontend validation

--- src/train.ts
+++ src/train.ts
@@ -1,12 +1,21 @@
-function palindromCheck(text: string): boolean {
-  for (let i = 0; i < text.length / 2; i++) {
-    if (text[i] !== text[text.length - 1 - i]) {
-      return false;
-    }
-  }
-  return true;
+// TASK O:
+
+// Shunday function yozing va u har xil qiymatlardan iborat array qabul qilsin.
+// Va array ichidagi sonlar yig'indisini hisoblab chiqgan javobni qaytarsin
+
+// MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]); return 45
+
+// Yuqoridagi misolda array tarkibida faqatgina ikkita yagona son mavjud bular 10 hamda 35
+// Qolganlari nested bo'lib yoki type'lari number emas.
+
+
+
+
+function calculateSumOfNumbers(arr: any[]): number {
+  return arr.reduce((sum, item) => {
+    return typeof item === "number" ? sum + item : sum;
+  }, 0);
 }
 
-
-console.log(palindromCheck("dad")); 
-console.log(palindromCheck("son")); 
+const result = calculateSumOfNumbers([10, "10", {son: 10}, true, 35]);
+console.log(result); 
