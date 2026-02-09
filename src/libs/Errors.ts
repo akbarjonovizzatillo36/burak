1ac090d61d1bf99daa8827d91ed169456c8f18e9
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-08 18:01:09 -0800

    fix: modify spa login rest api process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -17,6 +17,7 @@ export enum Message {
 
   USED_NICK_PHONE = "You're inserting already used nick or phone!",
   NO_MEMBER_NICK = "No Member with that nick!",
+  BLOCKED_USER = "You have been blocked, contact restaurant!",
   WRONG_PASSWORD = "Wrong password!",
   NOT_AUTHENTICATED = "You are not authenticated, please login!",
 }
