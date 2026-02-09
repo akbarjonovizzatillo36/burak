9b3798f08e86cebf68c9e51a7c52748d5ca312cc
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-08 18:31:07 -0800

    fix: admin project refactoring

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,12 @@
+// TASK P:
+
+// Parametr sifatida yagona object qabul qiladigan function yozing.
+// Qabul qilingan objectni nested array sifatida convert qilib qaytarsin
+
+// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+
+
+
 function objectToArray(obj: Record<string, any>): [string, any][] {
     return Object.entries(obj);
 }
