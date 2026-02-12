13b4f6ec97769180c0f7147097e3a8754f93940e
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-11 19:27:09 -0800

    feat: TASK Q solution

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,5 @@
-// TASK P:
-
-// Parametr sifatida yagona object qabul qiladigan function yozing.
-// Qabul qilingan objectni nested array sifatida convert qilib qaytarsin
-
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
-
-
-
-function objectToArray(obj: Record<string, any>): [string, any][] {
-    return Object.entries(obj);
+function hasProperty(obj: object, key: string): boolean {
+  return Object.keys(obj).includes(key);
 }
-
-const result = objectToArray({ a: 10, b: 20 });
-console.log(result);
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model")); // true
+console.log(hasProperty({ name: "BMW", model: "M3" }, "year"));  // false
