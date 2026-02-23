2a6ca89a0b53b19a67bbb5a38379b1a45280ec36
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-22 19:23:09 -0800

    feat Task V solution

--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,23 @@
-function sumOdds(n: number): number {
-  return Math.floor(n / 2);
+// TASK V
+
+// Shunday function yozing, uni string parametri bo'lsin.
+// Va bu function stringdagi har bir harfni o'zi bilan
+// necha marotaba taktorlanganligini ko'rsatuvchi object qaytarsin.
+  
+// MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
+
+// Yuqoridagi misolda, 'hello' so'zi tarkibida
+// qatnashgan harflar necha marotaba takrorlangini bilan
+// object sifatida qaytarilmoqda.
+
+function countChars(str: string): Record<string, number> {
+  const result: Record<string, number> = {};
+
+  for (const char of str) {
+    result[char] = (result[char] || 0) + 1;
+  }
+
+  return result;
 }
 
-console.log(sumOdds(9));   
-console.log(sumOdds(11));  
+console.log(countChars("hello"));
