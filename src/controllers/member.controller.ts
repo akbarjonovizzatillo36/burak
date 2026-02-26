1fec2f4444af6609aa56bd5a3a5e7f9c96e53206
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-25 18:27:23 -0800

    feat: develop authentication via tokens with cookies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from "express";
 import { T } from "../libs/types/common"
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import MemberService from "../models/Member.service";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 
 const memberService = new MemberService()
@@ -16,9 +17,13 @@ memberController.signup = async (req: Request, res: Response) => {
         const input: MemberInput = req.body,
             result: Member = await memberService.signup(input);
             const token = await authService.createToken(result);
-            //TOKENS
+            
+            res.cookie("accessToken", token, {
+              maxAge: AUTH_TIMER * 3600 * 1000,
+              httpOnly: false,
+        });
 
-        res.json({member: result});
+        res.status(HttpCode.CREATED).json({member: result, accessToken: token });
     } catch (err) {
         console.log('Error, signup:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
@@ -35,9 +40,12 @@ memberController.login = async(req: Request, res: Response) => {
             console.log("token =>", token);
             //TOKENS
 
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
 
-
-        res.json({member: result});
+        res.status(HttpCode.OK).json({member: result, accessToken: token });
     } catch (err) {
         console.log('Error, login:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
