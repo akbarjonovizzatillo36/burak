1676b5f99cebe5e015beca64cde4a52b396f996d
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-02-25 19:42:55 -0800

    feat: develop verify authenticated user credentials

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,16 @@ import express from "express";
 const router = express.Router();
 import memberController from './controllers/member.controller';
 
-//REACT
+/** Member **/
 
-router.post('/login', memberController.login);
-router.post('/signup', memberController.signup);
+router.post("/member/login", memberController.login);
+router.post("/member/signup", memberController.signup);
+router.get("/member/detail", memberController.verifyAuth);
+
+/** Product **/
+
+/** Order **/
+
 
 export default router;
 
