96676e34b322599b92b7ba91302ec5218e002a86
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-03-15 22:00:25 -0700

    task solution

--- src/train.ts
+++ src/train.ts
@@ -1,26 +1,6 @@
-// TASK ZD
-
-// Shunday function yozing. Bu function o'ziga, parametr sifatida
-// birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
-// qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
-// shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
-// yangilangan arrayni qaytarsin.
-
-// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
-
-// Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
-// Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
-// yangilangan arrayni qaytarmoqda.
-
-function changeNumberInArray(
-  index: number,
-  arr: number[],
-  newValue: number
-): number[] {
-
-  arr[index] = newValue;
-  return arr;
-
+function removeDuplicate(str: string): string {
+  return [...new Set(str)].join('');
 }
 
-console.log(changeNumberInArray(1, [1,3,7,2], 2));
+
+console.log(removeDuplicate('stringg')); 
