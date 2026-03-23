c8c137b8151a53acdc235466068dbd0044224b69
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-03-22 23:15:59 -0700

    Task ZG Solution

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,14 @@
-// TASK ZF
+// TASK ZG
 
+// String sifatida berilgan string parametrni
+// snake case'ga o'tkazib beradigan function yozing.
 
-// Shunday function yozing, uni string parametri bo'lsin.
-// Ushbu function, har bir so'zni bosh harflarini katta harf qilib qaytarsin.
-// Lekin uzunligi 1 yoki 2 harfga teng bo'lgan so'zlarni esa o'z holicha
-// qoldirsin.
+// MASALAN: convertToSnakeCase('name should be a string')
+// return 'name_should_be_a_string'
 
-// MASALAN: capitalizeWords('name should be a string'); return 'Name Should be a String';
-
-// Yuqoridagi misolda, bizning function, uzunligi 2 harfdan katta bo'lgan so'zlarnigina,
-// birinchi harfini katta harf bilan qaytarmoqda.
-
-function capitalizeWords(str: string): string {
-  return str
-    .split(" ")
-    .map((word) =>
-      word.length > 2 ? word[0].toUpperCase() + word.slice(1) : word
-    )
-    .join(" ");
+function convertToSnakeCase(str: string): string {
+  return str.split(" ").join("_");
 }
 
-console.log(capitalizeWords("name should be a string"));
+console.log(convertToSnakeCase("name should be a string"));
+
