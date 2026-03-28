7c9fe45b7caeb97622e87a101fe2b83ab0321418
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-03-27 20:57:17 -0700

    Task ZH Solution

--- src/train.ts
+++ src/train.ts
@@ -1,14 +1,23 @@
-// TASK ZG
+// TASK ZH
 
-// String sifatida berilgan string parametrni
-// snake case'ga o'tkazib beradigan function yozing.
+// Shunday function yozing, u berilgan array parametri ichidagi
+// raqamlar orasidan, tartib bo'yicha eng kichik raqamdan, eng katta raqamgacha
+// tushirib qoldirilgan sonlarni o'zinigina topib bir array sifatida qaytarsin.
+  
+// MASALAN: findDisappearedNumbers([1, 3, 4, 7]); return [2, 5, 6];
 
-// MASALAN: convertToSnakeCase('name should be a string')
-// return 'name_should_be_a_string'
+// Yuqoridagi misolda, eng katta raqam bu 7 va eng kichik raqam bu 1.
+// Function'ning vazifasi berilgan sonlar ichidan tushirib qoldirilgan
+// sonlarnigina topib qaytarmoqda.
 
-function convertToSnakeCase(str: string): string {
-  return str.split(" ").join("_");
+function findDisappearedNumbers(arr: number[]): number[] {
+  const res: number[] = [];
+
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    if (!arr.includes(i)) res.push(i);
+  }
+
+  return res;
 }
 
-console.log(convertToSnakeCase("name should be a string"));
-
+console.log(findDisappearedNumbers([1, 3, 4, 7]));
