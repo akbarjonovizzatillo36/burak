c947f008ade935b179c0f254d9209828a00f0647
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2026-03-29 19:21:45 -0700

    feat: Task ZI solution

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,18 @@
-// TASK ZH
+// //TASK ZI
 
-// Shunday function yozing, u berilgan array parametri ichidagi
-// raqamlar orasidan, tartib bo'yicha eng kichik raqamdan, eng katta raqamgacha
-// tushirib qoldirilgan sonlarni o'zinigina topib bir array sifatida qaytarsin.
-  
-// MASALAN: findDisappearedNumbers([1, 3, 4, 7]); return [2, 5, 6];
+// Shundan function yozing, bu function 3 soniydan so'ng
+// "Hello World!" so'zini qaytarsin.
 
-// Yuqoridagi misolda, eng katta raqam bu 7 va eng kichik raqam bu 1.
-// Function'ning vazifasi berilgan sonlar ichidan tushirib qoldirilgan
-// sonlarnigina topib qaytarmoqda.
+// MASALAN: delayHelloWorld("Hello World"); return "Hello World";
 
-function findDisappearedNumbers(arr: number[]): number[] {
-  const res: number[] = [];
-
-  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
-    if (!arr.includes(i)) res.push(i);
-  }
-
-  return res;
+function delayHelloWorld(text: string): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(text);
+    }, 3000);
+  });
 }
 
-console.log(findDisappearedNumbers([1, 3, 4, 7]));
+delayHelloWorld("Hello World!").then((res) => {
+  console.log(res);
+});
